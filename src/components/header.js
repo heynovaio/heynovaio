36fc59f6ae39f9e06fc6c7221bfa86abde4818a8
Kirsten Dodd <kirsten.jvd@gmail.com>
2020-04-06 13:21:16 -0700

    Font awesome added

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,27 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from '@emotion/styled'
 import img1 from '../images/logo-2-x.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faLinkedinIn, faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+
 const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 2;
   img {
-    width: 56px;
-    top:17px;
-    left: 49px;
-    position: fixed;
+    max-width: 56px;
+    margin-top:17px;
+    margin-left: 49px;
   }
+`
+const LinkContainer = styled.div`
+  display: flex;
   a {
+    padding: 20px 12px;
     font-size: 18px;
     font-family: Stolzl;
     font-weight: 500;
@@ -19,29 +30,31 @@ const Header = styled.header`
     color: #ffffff;
   }
 `
-const BlogLink = styled.a`
-  width: 41px;
-  height: 26px;
-  top: 26px;
-  right: 356px;
-  position: fixed;
+const FaceBookLink = styled.a`
+  color: #d5330d;
 `
-const ContactLink = styled.a`
-  width: 80px;
-  height: 26px;
-  top: 26px;
-  right: 213px;
-  position: fixed;
+const InstagramLink = styled.a`
+  color: #d5330d;
 `
-const FaceBookLink = styled.a``
-const InstagramLink = styled.a``
+const LinkedInLink = styled.a`
+  color: #d5330d;
+`
 const MailLink = styled.a``
 export default () => (
   <Header>
     <img src={img1}/>
-    <BlogLink>Blog</BlogLink>
-    <ContactLink><a>Contact</a></ContactLink>
-    <FaceBookLink></FaceBookLink>
-    <InstagramLink></InstagramLink>
+    <LinkContainer>
+      <Link to="/blog">Blog</Link>
+      <Link to="/contact">Contact</Link>
+      <LinkedInLink>
+         <FontAwesomeIcon icon={faLinkedinIn} />
+      </LinkedInLink>
+      <FaceBookLink>
+         <FontAwesomeIcon icon={faFacebook} />
+      </FaceBookLink>
+      <InstagramLink>
+         <FontAwesomeIcon icon={faInstagram} />
+      </InstagramLink>
+    </LinkContainer>
   </Header>
 )
