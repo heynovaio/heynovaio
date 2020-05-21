c8fb3038de70954dbf15b48ce1f8432e05b74031
Kirsten Dodd <kirsten.jvd@gmail.com>
2020-05-21 03:28:42 -0700

    Update seo meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import Footer from "./footer"
 import styled from '@emotion/styled'
-
+import SEO from "../seo"
 import "./layout.css"
 
 const Main = styled.main`
@@ -20,6 +20,11 @@ const Main = styled.main`
 const Layout = ({ children }) => {
   return (
     <>
+      <SEO
+        title="Hey Nova | Branding, Designing, Developing"
+        description="Evolving the way companies communicate to their audience by creating cohesive brands, user-centred design, and inclusive code."
+        keywords={[ `Web Design`, `Web Development`, `Victoria`, `British Columbia`, `Marketing`, `User Experience`, `Branding` ]}
+      />
       <Header></Header>
       <Main>{children}</Main>
       <Footer></Footer>
