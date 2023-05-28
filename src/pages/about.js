09eb882f8b25efb4cf1eabd5a783eb04efa78d00
Hal <haln_01@proton.me>
2023-05-27 17:36:50 -0700

    feat[wip]: templates for values section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Hero, Mission } from "../components/about"
+import { Hero, Mission, Values } from "../components/about"
 
 export default function About() {
   return (
     <Layout>
       <Hero />
       <Mission />
+      {/* <Team /> */}
+      <Values />
     </Layout>
   )
 }
