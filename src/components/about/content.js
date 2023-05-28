72d6a4e4dbf4525f9755bddbb746126ede2929c7
Hal <haln_01@proton.me>
2023-05-27 22:36:43 -0700

    feat: value sections

---
+++ src/components/about/content.js
@@ -0,0 +1,32 @@
+import WomenLed from "./assets/women-led.svg"
+import RemoteForever from "./assets/remote-forever.svg"
+import PurposeDrive from "./assets/purpose-driven.svg"
+import Inclusive from "./assets/inclusive-accessible.svg"
+
+// TODO: change content
+export const values = [
+  {
+    icon: WomenLed,
+    title: "Women Led",
+    content:
+      "Sit adipisicing provident ratione mollitia sed Deserunt provident molestiae tempora!",
+  },
+  {
+    icon: RemoteForever,
+    title: "Remote Forever",
+    content:
+      "Ipsum blanditiis quisquam doloribus praesentium necessitatibus Reiciendis neque repellendus atque",
+  },
+  {
+    icon: PurposeDrive,
+    title: "Purpose Driven",
+    content:
+      "Elit voluptatibus officiis officia minima illum animi. Esse id distinctio.",
+  },
+  {
+    icon: Inclusive,
+    title: "Inclusive & Accessible",
+    content:
+      "Dolor adipisicing nam cupiditate quisquam iste. Cupiditate repellat amet unde.",
+  },
+]
